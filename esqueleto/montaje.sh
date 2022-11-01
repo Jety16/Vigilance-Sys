7540942731c366fabba50639f5a8138882be4377
Tomas Bazan <tomas.pablo.bazan@mi.unc.edu.ar>
2022-11-01 09:03:37 -0300

    Agregando features al script

--- esqueleto/montaje.sh
+++ esqueleto/montaje.sh
@@ -2,9 +2,18 @@
 if [ "$1" == "mont" ]; then
     ./fat-fuse ../resources/bb_fs.img mnt
 fi
-if [ "$1" == "help" ]; then
-    echo -e " Para montar imagen correr './montaje.sh mont'.\n Para desmontar: './montaje.sh um'. \n Para ayuda: './mont.sh help'."
-fi
 if [ "$1" == "um" ]; then
     umount mnt
+fi
+if [ "$1" == "test" ]; then
+    umount mnt
+    make clean
+    make
+    ./fat-fuse ../resources/bb_fs.img mnt
+fi
+if [ "$1" == "help" ]; then
+    echo  " Para montar imagen correr './montaje.sh mont'.
+    Para desmontar: './montaje.sh um'.
+    Para testing: './montaje.sh test' (desmonta, make clean, make y monta de nuevo). 
+    Para ayuda: './mont.sh help'."
 fi
