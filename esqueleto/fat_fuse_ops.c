038de3f3855428b913d108db0ffa80fac0f371d3
Ignacio Scavuzzo <iscavuzzo@mi.unc.edu.ar>
2022-11-05 23:26:04 +0000

    unlink?

--- esqueleto/fat_fuse_ops.c
+++ esqueleto/fat_fuse_ops.c
@@ -6,6 +6,12 @@
 
 #include "fat_fuse_ops.h"
 
+#include "big_brother.h"
+#include "fat_file.h"
+#include "fat_filename_util.h"
+#include "fat_fs_tree.h"
+#include "fat_util.h"
+#include "fat_volume.h"
 #include <alloca.h>
 #include <errno.h>
 #include <gmodule.h>
@@ -16,340 +22,340 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include "big_brother.h"
-#include "fat_file.h"
-#include "fat_filename_util.h"
-#include "fat_fs_tree.h"
-#include "fat_util.h"
-#include "fat_volume.h"
 
 #define LOG_MESSAGE_SIZE 100
 #define DATE_MESSAGE_SIZE 30
 
-static void now_to_str(char *buf) {
-  time_t now = time(NULL);
-  struct tm *timeinfo;
-  timeinfo = localtime(&now);
+// static void now_to_str(char *buf) {
+//     time_t now = time(NULL);
+//     struct tm *timeinfo;
+//     timeinfo = localtime(&now);
 
-  strftime(buf, DATE_MESSAGE_SIZE, "%d-%m-%Y %H:%M", timeinfo);
-}
+//     strftime(buf, DATE_MESSAGE_SIZE, "%d-%m-%Y %H:%M", timeinfo);
+// }
 
-void fat_fuse_log_activity(char *operation_type, fat_file file, fat_volume fs_vol) {
-  // buf will save the data of the operation
+// static void fat_fuse_log_activity(char *operation_type, fat_file file) {
+//     char buf[LOG_MESSAGE_SIZE] = "";
+//     now_to_str(buf);
+//     strcat(buf, "\t");
+//     strcat(buf, getlogin());
+//     strcat(buf, "\t");
+//     strcat(buf, file->filepath);
+//     strcat(buf, "\t");
+//     strcat(buf, operation_type);
+//     strcat(buf, "\n");
+//     int message_size = strlen(buf);
+// }
 
-  DEBUG("\n\n LINEA 52 \n\n");
-
-  char buf[LOG_MESSAGE_SIZE] = "";
-  now_to_str(buf);
-  strcat(buf, "\t");
-  strcat(buf, getlogin());
-  strcat(buf, "\t");
-  strcat(buf, file->filepath);
-  strcat(buf, "\t");
-  strcat(buf, operation_type);
-  strcat(buf, "\n");
-  int message_size = strlen(buf);
-  off_t offset = 0;
-  // llamo funcion auxiliar que escriba en fs.log el bufer
-  // fat_file fs_log = fat_tree_search(tree, "/fs.log");
-  DEBUG("\n\n LINEA 52 \n\n");
-
-  fat_tree_node fs_node = fat_tree_node_search(fs_vol->file_tree, "/fs.log");
-  // fat_tree_node fs_node = (fat_tree_node)fi->fh;
-  DEBUG("\n\n LINEA 57 \n\n");
-  fat_file fs_log = fat_tree_get_file(fs_node);
-  fat_file fs_parent = fat_tree_get_parent(fs_node);
-  DEBUG("\n\n LINEA 60 \n\n");
-  // ver codigos de error
-
-  printf("%s\n\n", buf);
-  // ver como se calcula el offset
-  int result = fat_file_pwrite(fs_log, buf, message_size, offset, fs_parent);
-  if (result < 0) {
-    EXIT_FAILURE;
-  }
-}
-
-///* Write data from a file */
-// int fat_fuse_write_activity(const char *path, const char *buf, size_t size,
-//                            off_t offset, struct fuse_file_info *fi) {
-//  fat_tree_node file_node = (fat_tree_node)fi->fh;
-//  fat_file file = fat_tree_get_file(file_node);
-//  fat_file parent = fat_tree_get_parent(file_node);
-//
-//  // en casos de errores llamo a log_activity?
-//  if (size == 0) return 0;  // Nothing to write
-//  if (offset > file->dentry->file_size) return -EOVERFLOW;
-//  // llamo a log_activity
-//  return fat_file_pwrite(file, buf, size, offset, parent);
-//}
 
 /* Get file attributes (file descriptor version) */
 int fat_fuse_fgetattr(const char *path, struct stat *stbuf,
                       struct fuse_file_info *fi) {
-  fat_file file = (fat_file)fat_tree_get_file((fat_tree_node)fi->fh);
-  fat_file_to_stbuf(file, stbuf);
-  return 0;
+    fat_file file = (fat_file)fat_tree_get_file((fat_tree_node)fi->fh);
+    fat_file_to_stbuf(file, stbuf);
+    return 0;
 }
 
 /* Get file attributes (path version) */
 int fat_fuse_getattr(const char *path, struct stat *stbuf) {
-  fat_volume vol;
-  fat_file file;
+    fat_volume vol;
+    fat_file file;
 
-  vol = get_fat_volume();
-  file = fat_tree_search(vol->file_tree, path);
-  if (file == NULL) {
-    errno = ENOENT;
-    return -errno;
-  }
-  fat_file_to_stbuf(file, stbuf);
-  return 0;
+    vol = get_fat_volume();
+    file = fat_tree_search(vol->file_tree, path);
+    if (file == NULL) {
+        errno = ENOENT;
+        return -errno;
+    }
+    fat_file_to_stbuf(file, stbuf);
+    return 0;
 }
 
 /* Open a file */
 int fat_fuse_open(const char *path, struct fuse_file_info *fi) {
-  fat_volume vol;
-  fat_tree_node file_node;
-  fat_file file;
+    fat_volume vol;
+    fat_tree_node file_node;
+    fat_file file;
 
-  vol = get_fat_volume();
-  file_node = fat_tree_node_search(vol->file_tree, path);
-  if (!file_node) return -errno;
-  file = fat_tree_get_file(file_node);
-  if (fat_file_is_directory(file)) return -EISDIR;
-  fat_tree_inc_num_times_opened(file_node);
-  fi->fh = (uintptr_t)file_node;
-  return 0;
+    vol = get_fat_volume();
+    file_node = fat_tree_node_search(vol->file_tree, path);
+    if (!file_node)
+        return -errno;
+    file = fat_tree_get_file(file_node);
+    if (fat_file_is_directory(file))
+        return -EISDIR;
+    fat_tree_inc_num_times_opened(file_node);
+    fi->fh = (uintptr_t)file_node;
+    return 0;
 }
 
 /* Open a directory */
 int fat_fuse_opendir(const char *path, struct fuse_file_info *fi) {
-  fat_volume vol = NULL;
-  fat_tree_node file_node = NULL;
-  fat_file file = NULL;
+    fat_volume vol = NULL;
+    fat_tree_node file_node = NULL;
+    fat_file file = NULL;
 
-  vol = get_fat_volume();
-  file_node = fat_tree_node_search(vol->file_tree, path);
-  if (file_node == NULL) {
-    return -errno;
-  }
-  file = fat_tree_get_file(file_node);
-  if (!fat_file_is_directory(file)) {
-    return -ENOTDIR;
-  }
-  fat_tree_inc_num_times_opened(file_node);
-  fi->fh = (uintptr_t)file_node;
-  return 0;
+    vol = get_fat_volume();
+    file_node = fat_tree_node_search(vol->file_tree, path);
+    if (file_node == NULL) {
+        return -errno;
+    }
+    file = fat_tree_get_file(file_node);
+    if (!fat_file_is_directory(file)) {
+        return -ENOTDIR;
+    }
+    fat_tree_inc_num_times_opened(file_node);
+    fi->fh = (uintptr_t)file_node;
+    return 0;
 }
 
 /* Read directory children. Calls function fat_file_read_children which returns
  * a list of files inside a GList. The children were read from the directory
- * entries in the cluster of the directory.
- * This function iterates over the list of children and adds them to the
- * file tree.
+ * entries in the cluster of the directory. 
+ * This function iterates over the list of children and adds them to the 
+ * file tree. 
  * This operation should be performed only once per directory, the first time
  * readdir is called.
  */
 static void fat_fuse_read_children(fat_tree_node dir_node) {
-  fat_volume vol = get_fat_volume();
-  fat_file dir = fat_tree_get_file(dir_node);
-  GList *children_list = fat_file_read_children(dir);
-  // Add child to tree. TODO handle duplicates
-  for (GList *l = children_list; l != NULL; l = l->next) {
-    vol->file_tree =
-        fat_tree_insert(vol->file_tree, dir_node, (fat_file)l->data);
-  }
+    fat_volume vol = get_fat_volume();
+    fat_file dir = fat_tree_get_file(dir_node);
+    GList *children_list = fat_file_read_children(dir);
+    // Add child to tree. TODO handle duplicates
+    for (GList *l = children_list; l != NULL; l = l->next) {
+        vol->file_tree =
+            fat_tree_insert(vol->file_tree, dir_node, (fat_file)l->data);
+    }
 }
 
 /* Add entries of a directory in @fi to @buf using @filler function. */
 int fat_fuse_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
                      off_t offset, struct fuse_file_info *fi) {
-  errno = 0;
-  fat_tree_node dir_node = (fat_tree_node)fi->fh;
-  fat_file dir = fat_tree_get_file(dir_node);
-  fat_file *children = NULL, *child = NULL;
-  int error = 0;
+    errno = 0;
+    fat_tree_node dir_node = (fat_tree_node)fi->fh;
+    fat_file dir = fat_tree_get_file(dir_node);
+    fat_file *children = NULL, *child = NULL;
+    int error = 0;
 
-  // Insert first two filenames (. and ..)
-  if ((*filler)(buf, ".", NULL, 0) || (*filler)(buf, "..", NULL, 0)) {
-    return -errno;
-  }
-  if (!fat_file_is_directory(dir)) {
-    errno = ENOTDIR;
-    return -errno;
-  }
-  if (dir->children_read != 1) {
-    fat_fuse_read_children(dir_node);
-    if (errno < 0) {
-      return -errno;
+    // Insert first two filenames (. and ..)
+    if ((*filler)(buf, ".", NULL, 0) || (*filler)(buf, "..", NULL, 0)) {
+        return -errno;
     }
-  }
+    if (!fat_file_is_directory(dir)) {
+        errno = ENOTDIR;
+        return -errno;
+    }
+    if (dir->children_read != 1) {
+        fat_fuse_read_children(dir_node);
+        if (errno < 0) {
+            return -errno;
+        }
+    }
 
-  children = fat_tree_flatten_h_children(dir_node);
-  child = children;
-  while (*child != NULL) {
-    error = (*filler)(buf, (*child)->name, NULL, 0);
-    if (error != 0) {
-      return -errno;
+    children = fat_tree_flatten_h_children(dir_node);
+    child = children;
+    while (*child != NULL) {
+        error = (*filler)(buf, (*child)->name, NULL, 0);
+        if (error != 0) {
+            return -errno;
+        }
+        child++;
     }
-    child++;
-  }
-  return 0;
+    return 0;
 }
 
 /* Read data from a file */
 int fat_fuse_read(const char *path, char *buf, size_t size, off_t offset,
                   struct fuse_file_info *fi) {
-  errno = 0;
-  int bytes_read;
-  fat_tree_node file_node = (fat_tree_node)fi->fh;
-  fat_file file = fat_tree_get_file(file_node);
-  fat_file parent = fat_tree_get_parent(file_node);  
-  fat_volume vol;
-  vol = get_fat_volume();
+    errno = 0;
+    int bytes_read;
+    fat_tree_node file_node = (fat_tree_node)fi->fh;
+    fat_file file = fat_tree_get_file(file_node);
+    fat_file parent = fat_tree_get_parent(file_node);
 
-  DEBUG("\n\n PRE LOG ACTIVITY \n\n");
-  fat_fuse_log_activity("Read", file, vol);
-  DEBUG("\n\n AFTER LOG ACTIVITY \n\n");
+    bytes_read = fat_file_pread(file, buf, size, offset, parent);
+    if (errno != 0) {
+        return -errno;
+    }
 
-  bytes_read = fat_file_pread(file, buf, size, offset, parent);
-  if (errno != 0) {
-    // acá tmb debería llamar a log_activity
-    return -errno;
-  }
-
-  // llamo a log_activity
-  return bytes_read;
+    return bytes_read;
 }
 
 /* Write data from a file */
 int fat_fuse_write(const char *path, const char *buf, size_t size, off_t offset,
                    struct fuse_file_info *fi) {
-  fat_tree_node file_node = (fat_tree_node)fi->fh;
-  fat_file file = fat_tree_get_file(file_node);
-  fat_file parent = fat_tree_get_parent(file_node);
-  fat_volume vol;
-  vol = get_fat_volume();
+    fat_tree_node file_node = (fat_tree_node)fi->fh;
+    fat_file file = fat_tree_get_file(file_node);
+    fat_file parent = fat_tree_get_parent(file_node);
 
-  // en casos de errores llamo a log_activity?
-  if (size == 0) return 0;  // Nothing to write
-  if (offset > file->dentry->file_size) return -EOVERFLOW;
-  // llamo a log_activity
-  fat_fuse_log_activity("Write", file, vol);
-  return fat_file_pwrite(file, buf, size, offset, parent);
+    if (size == 0)
+        return 0; // Nothing to write
+    if (offset > file->dentry->file_size)
+        return -EOVERFLOW;
+
+    return fat_file_pwrite(file, buf, size, offset, parent);
 }
 
 /* Close a file */
 int fat_fuse_release(const char *path, struct fuse_file_info *fi) {
-  fat_tree_node file = (fat_tree_node)fi->fh;
-  fat_tree_dec_num_times_opened(file);
-  return 0;
+    fat_tree_node file = (fat_tree_node)fi->fh;
+    fat_tree_dec_num_times_opened(file);
+    return 0;
 }
 
 /* Close a directory */
 int fat_fuse_releasedir(const char *path, struct fuse_file_info *fi) {
-  fat_tree_node file = (fat_tree_node)fi->fh;
-  fat_tree_dec_num_times_opened(file);
-  return 0;
+    fat_tree_node file = (fat_tree_node)fi->fh;
+    fat_tree_dec_num_times_opened(file);
+    return 0;
 }
 
 int fat_fuse_mkdir(const char *path, mode_t mode) {
-  errno = 0;
-  fat_volume vol = NULL;
-  fat_file parent = NULL, new_file = NULL;
-  fat_tree_node parent_node = NULL;
+    errno = 0;
+    fat_volume vol = NULL;
+    fat_file parent = NULL, new_file = NULL;
+    fat_tree_node parent_node = NULL;
 
-  // The system has already checked the path does not exist. We get the parent
-  vol = get_fat_volume();
-  parent_node = fat_tree_node_search(vol->file_tree, dirname(strdup(path)));
-  if (parent_node == NULL) {
-    errno = ENOENT;
-    return -errno;
-  }
-  parent = fat_tree_get_file(parent_node);
-  if (!fat_file_is_directory(parent)) {
-    fat_error("Error! Parent is not directory\n");
-    errno = ENOTDIR;
-    return -errno;
-  }
+    // The system has already checked the path does not exist. We get the parent
+    vol = get_fat_volume();
+    parent_node = fat_tree_node_search(vol->file_tree, dirname(strdup(path)));
+    if (parent_node == NULL) {
+        errno = ENOENT;
+        return -errno;
+    }
+    parent = fat_tree_get_file(parent_node);
+    if (!fat_file_is_directory(parent)) {
+        fat_error("Error! Parent is not directory\n");
+        errno = ENOTDIR;
+        return -errno;
+    }
 
-  // init child
-  new_file = fat_file_init(vol->table, true, strdup(path));
-  if (errno != 0) {
+    // init child
+    new_file = fat_file_init(vol->table, true, strdup(path));
+    if (errno != 0) {
+        return -errno;
+    }
+    // insert to directory tree representation
+    vol->file_tree = fat_tree_insert(vol->file_tree, parent_node, new_file);
+    // write file in parent's entry (disk)
+    fat_file_dentry_add_child(parent, new_file);
     return -errno;
-  }
-  // insert to directory tree representation
-  vol->file_tree = fat_tree_insert(vol->file_tree, parent_node, new_file);
-  // write file in parent's entry (disk)
-  fat_file_dentry_add_child(parent, new_file);
-  return -errno;
 }
 
 /* Creates a new file in @path. @mode and @dev are ignored. */
 int fat_fuse_mknod(const char *path, mode_t mode, dev_t dev) {
-  errno = 0;
-  fat_volume vol;
-  fat_file parent, new_file;
-  fat_tree_node parent_node;
+    errno = 0;
+    fat_volume vol;
+    fat_file parent, new_file;
+    fat_tree_node parent_node;
 
-  // The system has already checked the path does not exist. We get the parent
-  vol = get_fat_volume();
-  parent_node = fat_tree_node_search(vol->file_tree, dirname(strdup(path)));
-  if (parent_node == NULL) {
-    errno = ENOENT;
+    // The system has already checked the path does not exist. We get the parent
+    vol = get_fat_volume();
+    parent_node = fat_tree_node_search(vol->file_tree, dirname(strdup(path)));
+    if (parent_node == NULL) {
+        errno = ENOENT;
+        return -errno;
+    }
+    parent = fat_tree_get_file(parent_node);
+    if (!fat_file_is_directory(parent)) {
+        fat_error("Error! Parent is not directory\n");
+        errno = ENOTDIR;
+        return -errno;
+    }
+    new_file = fat_file_init(vol->table, false, strdup(path));
+    if (errno < 0) {
+        return -errno;
+    }
+    // insert to directory tree representation
+    vol->file_tree = fat_tree_insert(vol->file_tree, parent_node, new_file);
+    // Write dentry in parent cluster
+    fat_file_dentry_add_child(parent, new_file);
     return -errno;
-  }
-  parent = fat_tree_get_file(parent_node);
-  if (!fat_file_is_directory(parent)) {
-    fat_error("Error! Parent is not directory\n");
-    errno = ENOTDIR;
-    return -errno;
-  }
-  new_file = fat_file_init(vol->table, false, strdup(path));
-  if (errno < 0) {
-    return -errno;
-  }
-  // insert to directory tree representation
-  vol->file_tree = fat_tree_insert(vol->file_tree, parent_node, new_file);
-  // Write dentry in parent cluster
-  fat_file_dentry_add_child(parent, new_file);
-  return -errno;
 }
 
 int fat_fuse_utime(const char *path, struct utimbuf *buf) {
-  errno = 0;
-  fat_file parent = NULL;
-  fat_volume vol = get_fat_volume();
-  fat_tree_node file_node = fat_tree_node_search(vol->file_tree, path);
-  if (file_node == NULL || errno != 0) {
-    errno = ENOENT;
+    errno = 0;
+    fat_file parent = NULL;
+    fat_volume vol = get_fat_volume();
+    fat_tree_node file_node = fat_tree_node_search(vol->file_tree, path);
+    if (file_node == NULL || errno != 0) {
+        errno = ENOENT;
+        return -errno;
+    }
+    parent = fat_tree_get_parent(file_node);
+    if (parent == NULL || errno != 0) {
+        DEBUG("WARNING: Setting time for parent ignored");
+        return 0; // We do nothing, no utime for parent
+    }
+    fat_utime(fat_tree_get_file(file_node), parent, buf);
     return -errno;
-  }
-  parent = fat_tree_get_parent(file_node);
-  if (parent == NULL || errno != 0) {
-    DEBUG("WARNING: Setting time for parent ignored");
-    return 0;  // We do nothing, no utime for parent
-  }
-  fat_utime(fat_tree_get_file(file_node), parent, buf);
-  return -errno;
 }
 
 /* Shortens the file at the given offset.*/
 int fat_fuse_truncate(const char *path, off_t offset) {
-  errno = 0;
-  fat_volume vol = get_fat_volume();
-  fat_file file = NULL, parent = NULL;
-  fat_tree_node file_node = fat_tree_node_search(vol->file_tree, path);
-  if (file_node == NULL || errno != 0) {
-    errno = ENOENT;
-    return -errno;
-  }
-  file = fat_tree_get_file(file_node);
-  if (fat_file_is_directory(file)) return -EISDIR;
+    errno = 0;
+    fat_volume vol = get_fat_volume();
+    fat_file file = NULL, parent = NULL;
+    fat_tree_node file_node = fat_tree_node_search(vol->file_tree, path);
+    if (file_node == NULL || errno != 0) {
+        errno = ENOENT;
+        return -errno;
+    }
+    file = fat_tree_get_file(file_node);
+    if (fat_file_is_directory(file))
+        return -EISDIR;
 
-  parent = fat_tree_get_parent(file_node);
-  fat_tree_inc_num_times_opened(file_node);
-  fat_file_truncate(file, offset, parent);
-  return -errno;
+    parent = fat_tree_get_parent(file_node);
+    fat_tree_inc_num_times_opened(file_node);
+    fat_file_truncate(file, offset, parent);
+    return -errno;
+}
+
+
+int fat_fuse_unlink(const char *path){
+    errno = 0;
+
+    //CHECKEAR QUE SEA UN ARCHIVOOOOOO  
+    
+    fat_volume vol = get_fat_volume();
+    fat_tree_node file_node = fat_tree_node_search(vol->file_tree, path);
+    
+    if (file_node == NULL || errno != 0) {
+        errno = ENOENT;
+        return -errno;
+    }
+    fat_file file = fat_tree_get_file(file_node);
+    //dir entry 
+    fat_file parent = fat_tree_get_parent(file_node);
+    //file->dentry->base_name[0] = FAT_FILENAME_DELETED_CHAR;
+    
+    write_dir_entry(parent, file);
+
+    //ESTA PARTE ESTA MARCANDO COMO LIBRES TODOS LOS CLUSTERS
+    fat_fuse_truncate(path,0); // se marca como libre el primer cluster?
+    //u32 cluster = file->start_cluster;
+    //file->table[cluster] = 0; //borrar de la tabla el primer cluster
+                                                       //marco como libre el primer cluster
+    //  todo esto es parte de la FAT table
+
+
+
+    
+    
+    
+    //parent->dentry[file->pos_in_parent]= 0x00000000;
+    
+    
+
+
+
+
+    //Actualizar el arbol de directorios 
+    fat_tree_delete(vol->file_tree, path);
+    
+    return -errno;
+
+
+
+
 }
